2328d619b132f85aa63ad6ec096f2da60a459857
Federico Enriquez <fedenri98@gmail.com>
2021-08-16 23:27:45 -0300

    jumbotron details added, closing feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,5 @@
-import Jumbotron from './components/jumbotron';
-import jumbotronData from './fixtures/jumbo';
+import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
-  return (
-    <Jumbotron.Container>
-      {jumbotronData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>
-              <p>{item.title}</p>
-            </Jumbotron.Title>
-            <Jumbotron.SubTitle>
-              <p>{item.subTitle}</p>
-            </Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.alt} />
-          </Jumbotron.Pane>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-  );
+  return <JumbotronContainer />;
 }
